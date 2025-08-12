fe7e2bae7c64419427006fe63944da7ae346275f
jani4343 <ja7229562@gmail.com>
2025-08-12 22:45:30 +0500

    Update config.js

--- config.js
+++ config.js
@@ -77,6 +77,6 @@ DEV: process.env.DEV || "923237045919",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
